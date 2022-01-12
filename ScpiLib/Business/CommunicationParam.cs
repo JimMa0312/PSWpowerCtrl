58c4ff5bb2a5b5a716f73c7fed40ead3bc741119
马玉琛 <JimMa0312@outlook.com>
2022-01-12 12:55:39 +0800

    修改TargetInfo类聚合设计

---
+++ ScpiLib/Business/CommunicationParam.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO.Ports;
+
+namespace ScpiLib.Business
+{
+    public interface ICommunicationParam
+    {
+
+    }
+
+    public sealed class NetWorkCommunicationParam: ICommunicationParam
+    {
+        public string IpAddress { get; set; }
+        public int Port { get; set; }
+    }
+
+    public sealed class TtysComunicationParam: ICommunicationParam
+    {
+        public string PortName { get; set; }
+        public int Baudrate { get; set; }
+        public StopBits StopBits { get; set; }
+        public int DataBits { get; set; }
+        public Parity CheckSum { get; set; }
+    }
+}
--- ScpiLib/Business/TargetInfo.cs
+++ ScpiLib/Business/TargetInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Text;
 
 namespace ScpiLib.Business
@@ -7,9 +8,21 @@ namespace ScpiLib.Business
     [Serializable]
     public class TargetInfo
     {
-        public string name { get; set; }
-        public CommunicationType communType { get; set; }
-        public string[] commParam { get; set; }
+        /// <summary>
+        /// 远端设备名称
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 使用的通信方式
+        /// </summary>
+        public CommunicationType CommunType { get; set; }
+        /// <summary>
+        /// 通信参数
+        /// 
+        /// 如果CommuType是 NetWork 则占用2个参数
+        /// 如果CommuType是 NetWork 则占用2个参数
+        /// </summary>
+        public ICommunicationParam CommParam { get; set; }
 
         public enum CommunicationType
         {
@@ -19,27 +32,27 @@ namespace ScpiLib.Business
 
         public void setNetWorkConnectParam(string _targetIpV4Address, int _port)
         {
-            if(commParam==null)
-            {
-                commParam = new string[2];
-            }
+            CommunType = CommunicationType.NetWork;
 
-            commParam[0] = _targetIpV4Address;
-            commParam[1] = _port.ToString();
+            CommParam = new NetWorkCommunicationParam
+            {
+                IpAddress = _targetIpV4Address,
+                Port = _port
+            };
         }
 
         public void setTtyConnectParam(string _portName, int _baudRate, int _dataSize, float _stopSize, int _checkSum)
         {
-            if(commParam == null)
-            {
-                commParam = new string[5];
-            }
+            CommunType = CommunicationType.Tty;
 
-            commParam[0] = _portName;
-            commParam[1] = _baudRate.ToString();
-            commParam[2] = _dataSize.ToString();
-            commParam[3] = _stopSize.ToString();
-            commParam[4] = _checkSum.ToString();
+            CommParam = new TtysComunicationParam
+            {
+                PortName = _portName,
+                Baudrate = _baudRate,
+                StopBits = (StopBits)(_stopSize == 1.5 ? 3 : _stopSize),
+                DataBits = _dataSize,
+                CheckSum = (Parity)_checkSum
+            };
         }
     }
 }
